ef57d9b499c770feffbcf72aa661abff8346269b
YeKanStWe <erbottini@hotmail.com>
2025-07-07 09:23:10 -0300

    Comitando tarefa wrapper.

---
+++ Tarefa_Wrapper/TarefaWrapper.java
@@ -0,0 +1,11 @@
+
+
+public class TarefaWrapper {
+
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        Integer wrappedNumero1 = numero1;
+        System.out.println("Integer: " + wrappedNumero1);
+    }
+}
