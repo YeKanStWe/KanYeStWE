ff697c101cc2c60bf23accee392bb223e040f0a8
YeKanStWe <erbottini@hotmail.com>
2025-11-18 15:45:32 -0300

    tarefa

---
+++ tarefa_annotation/src/annotation1/Nometabela.java
@@ -0,0 +1,14 @@
+package annotation1;
+
+public class Nometabela {
+	public static void main(String[] args) {
+		nometabela();
+	}
+
+	private static void nometabela() {
+		System.out.println(Tabela.nametabela);
+		
+		
+	}
+
+}
