00ac4dcc550b90c2198289fc08e095076d12e544
YeKanStWe <erbottini@hotmail.com>
2025-12-04 12:12:40 -0300

    Add files via upload

---
+++ tarefa_testesparte2/src/br/com/rpires/dao/ClienteDao.java
@@ -0,0 +1,11 @@
+package br.com.rpires.dao;
+
+/**
+ * @author rodrigo.pires
+ */
+public class ClienteDao implements IClienteDao {
+
+    public String salvar() {
+        return "Sucesso";
+    }
+}
