8a906e532eadd99c6b516302d57dd6beb35a82e1
YeKanStWe <erbottini@hotmail.com>
2025-12-11 14:16:44 -0300

    Add files via upload

---
+++ tarefa_projeto3pt2/src/main/dao/generic/IGenericDAO.java
@@ -0,0 +1,25 @@
+package main.dao.generic;
+
+import main.dao.Persistente;
+import main.exceptions.DAOException;
+import main.exceptions.MaisDeUmRegistroException;
+import main.exceptions.TableException;
+import main.exceptions.TipoChaveNaoEncontradaException;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+
+public interface IGenericDAO <T extends Persistente, E extends Serializable> {
+
+    public Boolean cadastrar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+
+    public void excluir(E valor) throws DAOException;
+
+    public void alterar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+
+    public T consultar(E valor) throws MaisDeUmRegistroException, TableException, DAOException;
+
+    public Collection<T> buscarTodos() throws DAOException;
+}
+
---
+++ tarefa_projeto3pt2/src/test/dao/ClienteDaoMock.java
@@ -0,0 +1,39 @@
+package test.dao;
+
+import java.util.Collection;
+
+import main.dao.IClienteDAO;
+import main.domain.Cliente;
+import main.exceptions.TipoChaveNaoEncontradaException;
+
+public class ClienteDaoMock implements IClienteDAO {
+
+    @Override
+    public Boolean cadastrar(Cliente entity) throws TipoChaveNaoEncontradaException {
+        return true;
+    }
+
+    @Override
+    public void excluir(Long valor) {
+    }
+
+    @Override
+    public void alterar(Cliente entity) throws TipoChaveNaoEncontradaException {
+
+    }
+
+    @Override
+    public Cliente consultar(Long valor) {
+        Cliente cliente = new Cliente();
+        cliente.setCpf(valor);
+        return cliente;
+    }
+
+    @Override
+    public Collection<Cliente> buscarTodos() {
+        return null;
+    }
+
+
+}
+
