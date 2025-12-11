249deabb6f49a7b9e8551fefd0e3947021d27463
YeKanStWe <erbottini@hotmail.com>
2025-12-11 14:15:33 -0300

    Delete tarefa_projeto3pt2 directory

--- tarefa_projeto3pt2/src/main/dao/IProdutoDAO.java
+++
@@ -1,9 +0,0 @@
-package main.dao;
-
-import main.dao.generic.IGenericDAO;
-import main.domain.Produto;
-
-public interface IProdutoDAO extends IGenericDAO<Produto, String>{
-
-}
-
--- tarefa_projeto3pt2/src/main/services/IProdutoService.java
+++
@@ -1,8 +0,0 @@
-package main.services;
-
-import main.domain.Produto;
-import main.services.generic.IGenericService;
-
-public interface IProdutoService extends IGenericService<Produto, String> {
-
-}
