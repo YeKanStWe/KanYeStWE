f060bc50188c01b1225d1e4065125457e03ea768
YeKanStWe <erbottini@hotmail.com>
2025-12-16 11:55:49 -0300

    Add files via upload

---
+++ tarefa_hibernate_32/src/main/java/com/tarefahibernate/TarefaHibernate32Application.java
@@ -0,0 +1,13 @@
+package com.tarefahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaHibernate32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarefaHibernate32Application.class, args);
+    }
+
+}
---
+++ tarefa_hibernate_32/src/test/java/com/tarefahibernate/TarefaHibernate32ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaHibernate32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
