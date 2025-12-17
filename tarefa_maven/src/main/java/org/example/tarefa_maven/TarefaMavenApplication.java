2caee19f3778265f8f8be6ee148b4666e78e2a51
YeKanStWe <erbottini@hotmail.com>
2025-12-17 17:02:26 -0300

    Add files via upload

---
+++ tarefa_maven/src/main/java/org/example/tarefa_maven/TarefaMavenApplication.java
@@ -0,0 +1,13 @@
+package org.example.tarefa_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarefaMavenApplication.class, args);
+    }
+
+}
---
+++ tarefa_maven/src/test/java/org/example/tarefa_maven/TarefaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tarefa_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
