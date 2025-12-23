1be911265eacb0e80541b39eae8e7a290682b085
YeKanStWe <erbottini@hotmail.com>
2025-12-23 12:24:34 -0300

    Delete projetofinal directory

--- projetofinal/microservicoMemes/src/main/java/com/ebottini/microservicomemes/memes/repositories/CategoriaMemeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebottini.microservicomemes.memes.repositories;
-
-import com.ebottini.microservicomemes.memes.entities.CategoriaMeme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaMemeRepository extends JpaRepository<CategoriaMeme, Long> {
-}
--- projetofinal/microservicoMemes/src/main/java/com/ebottini/microservicomemes/memes/repositories/MemeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ebottini.microservicomemes.memes.repositories;
-
-import com.ebottini.microservicomemes.memes.entities.Meme;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemeRepository extends JpaRepository<Meme, Long> {
-}
